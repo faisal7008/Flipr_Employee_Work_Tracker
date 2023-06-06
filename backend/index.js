db1455ec2cd4a9c9007a07017237e6aecda8301e
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-07 00:16:13 +0530

    setting up frontend and backend libraries

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log("MongoDB is Connected...");
+        // Start server
+        app.listen(process.env.PORT || 9000, () =>
+          console.log("Server started")
+        );
+      })
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+// Routes
