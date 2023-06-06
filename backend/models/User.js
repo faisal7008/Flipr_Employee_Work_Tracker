e285d050049d48690acbe0d1dcec4e8bd8290bad
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-07 00:43:59 +0530

    created user modal for both admin and employee

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  contactNumber: {
+    type: String,
+    required: true
+  },
+  department: {
+    type: String,
+    required: true
+  },
+  joiningDate: {
+    type: Date,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'employee']
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
