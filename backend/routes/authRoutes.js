4c54c724fbffeabe730d15a16f61b7fe943d47be
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-07 00:46:02 +0530

    added auth middleware to secure routing

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,11 +7,12 @@ const {
   updateMe,
   deleteMe,
 } = require("../controllers/authController");
+const auth = require("../middlewares/authMiddleware");
 
 // http://localhost:9000/auth
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.route("/me").get(getMe).put(updateMe).delete(deleteMe);
+router.route("/me").get(auth, getMe).put(auth, updateMe).delete(auth, deleteMe);
 
 module.exports = router;
