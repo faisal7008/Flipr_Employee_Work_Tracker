0dee81c1ed0f59e8c82fbc7178d47b0f081280de
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-08 21:53:27 +0530

    added dashboards for admin and employee

---
+++ frontend/src/pages/Employee/Dashboard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../features/auth/authSlice';
+
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  return (
+    <div className=''>
+      <div className='text-sm breadcrumbs'>
+        <ul>
+          <li>
+            <a>Employee</a>
+          </li>
+          <li>
+            <a>Dashboard</a>
+          </li>
+        </ul>
+      </div>
+      <h2 className=' text-2xl font-semibold tracking-wide'>Dashboard</h2>
+    </div>
+  );
+}
