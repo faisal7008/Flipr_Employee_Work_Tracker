a1ef3809228d62a9a41b92c9cc37f1640454cf50
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-08 21:31:20 +0530

    added role based routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
-// import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import AdminLayout from './components/AdminLayout';
+import EmployeeLayout from './components/EmployeeLayout';
+import Login from './pages/Login';
+import { useSelector } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
-    <div className="App">
-      
+    <div className='App bg-slate-100'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route
+            path='/admin/*'
+            element={user && user.role === 'admin' ? <AdminLayout /> : <Navigate to={'/'} />}
+          />
+          <Route
+            path='/employee/*'
+            element={user && user.role === 'employee' ? <EmployeeLayout /> : <Navigate to={'/'} />}
+          />
+        </Routes>
+      </Router>
+      <ToastContainer autoClose={1000} toastContainerStyle={{ width: '320px' }} />
     </div>
   );
 }
