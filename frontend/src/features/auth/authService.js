34147b101f5a15a8d11e3e706206db472057e52e
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-08 21:50:39 +0530

    added authService and authSlice and auth state in redux store

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,126 @@
+import axios from 'axios';
+
+const api_url = `${process.env.REACT_APP_API_URL}`;
+
+// Register a new user
+export const registerUser = async (token, userData) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    console.log(config);
+    const response = await axios.post(api_url + '/auth/register', userData, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Login a user
+export const loginUser = async (userData) => {
+  try {
+    console.log(userData);
+    const response = await axios.post(api_url + '/auth/login', userData);
+    if (response.data) {
+      localStorage.setItem('user', JSON.stringify(response.data.user));
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    }
+    return response.data;
+  } catch (error) {
+    // console.log(error)
+    throw error;
+  }
+};
+
+// Get all employees
+export const getAllEmployees = async (token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.get(api_url + '/auth/employees', config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get current user's profile
+export const getMe = async (token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.get(api_url + '/auth/me', config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Update current user's profile
+export const updateMe = async (token, userData) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.put(api_url + '/auth/me', userData, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Get user by ID (Admin)
+export const getUserById = async (token, userId) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.get(api_url + `/auth/users/${userId}`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Update user by ID (Admin)
+export const updateUserById = async (token, userId, userData) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.put(api_url + `/auth/users/${userId}`, userData, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Delete user by ID (Admin)
+export const deleteUserById = async (token, userId) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    console.log(userId);
+    const response = await axios.delete(api_url + `/auth/users/${userId}`, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,247 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import * as authService from './authService';
+
+// Thunk action to register a new user
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (userData, { rejectWithValue, getState }) => {
+    try {
+      const token = getState().auth.token;
+      console.log(token);
+      const response = await authService.registerUser(token, userData);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to login a user
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (userData, { rejectWithValue }) => {
+    try {
+      // console.log(userData)
+      const response = await authService.loginUser(userData);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to get all employees
+export const getAllEmployees = createAsyncThunk(
+  'auth/getAllEmployees',
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await authService.getAllEmployees(token);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to get current user's profile
+export const getMe = createAsyncThunk('auth/getMe', async (_, { rejectWithValue, getState }) => {
+  try {
+    const token = getState().auth.token;
+    const response = await authService.getMe(token);
+    return response;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+});
+
+// Thunk action to update current user's profile
+export const updateMe = createAsyncThunk(
+  'auth/updateMe',
+  async (userData, { rejectWithValue, getState }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await authService.updateMe(token, userData);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to get user by ID (Admin)
+export const getUserById = createAsyncThunk(
+  'auth/getUserById',
+  async (userId, { rejectWithValue, getState }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await authService.getUserById(token, userId);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to update user by ID (Admin)
+export const updateUserById = createAsyncThunk(
+  'auth/updateUserById',
+  async ({ userId, userData }, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await authService.updateUserById(token, userId, userData);
+      dispatch(getAllEmployees());
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Thunk action to delete user by ID (Admin)
+export const deleteUserById = createAsyncThunk(
+  'auth/deleteUserById',
+  async (userId, { rejectWithValue, getState }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await authService.deleteUserById(token, userId);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const user = JSON.parse(localStorage.getItem('user'));
+const token = JSON.parse(localStorage.getItem('token'));
+
+const initialState = {
+  user: user || null,
+  employees: [],
+  isAuthenticated: !!user,
+  token: token || null,
+  loading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    clearError: (state) => {
+      state.error = null;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+      state.employees = null;
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.employees.push(action.payload.user);
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isAuthenticated = true;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(getAllEmployees.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getAllEmployees.fulfilled, (state, action) => {
+        state.loading = false;
+        state.employees = action.payload;
+      })
+      .addCase(getAllEmployees.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(getMe.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getMe.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(getMe.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(updateMe.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateMe.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(updateMe.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(getUserById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getUserById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(getUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(updateUserById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateUserById.fulfilled, (state) => {
+        state.loading = false;
+      })
+      .addCase(updateUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+      .addCase(deleteUserById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteUserById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.employees = state.employees.filter((emp) => emp._id !== action.payload.id);
+      })
+      .addCase(deleteUserById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      });
+  },
+});
+
+export const { clearError, logout } = authSlice.actions;
+
+export default authSlice.reducer;
