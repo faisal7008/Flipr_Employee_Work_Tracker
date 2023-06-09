fd1d60cbf52b5012ee31f808f9b2fefb3c1f1b26
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-09 14:23:08 +0530

    added toggle password visibility in login and register pages, reset state after form submit

--- frontend/src/components/Admin/RegisterModal.jsx
+++ frontend/src/components/Admin/RegisterModal.jsx
@@ -9,6 +9,7 @@ export default function RegisterModal() {
   const [contactNumber, setContactNumber] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const closeButtonRef = useRef(null);
@@ -50,19 +51,19 @@ export default function RegisterModal() {
     return true;
   };
 
-  const closeModal = async () => {
-    // Click the close button
-    if (closeButtonRef.current) {
-      closeButtonRef.current.click();
-    }
-  };
-
-  const resetState = async () => {
+  const resetState = () => {
     setName('');
     setEmail('');
     setDepartment('');
     setContactNumber('');
     setPassword('');
+  };
+
+  const closeModal = async () => {
+    // Click the close button
+    if (closeButtonRef.current) {
+      closeButtonRef.current.click();
+    }
   };
 
   const handleSubmit = async () => {
@@ -83,6 +84,11 @@ export default function RegisterModal() {
       setErrorMsg('contactNumber');
       return;
     }
+    const contactRegex = /^\d{10}$/;
+    if (!contactRegex.test(contactNumber)) {
+      setErrorMsg('invalidContact');
+      return;
+    }
     if (!department) {
       setErrorMsg('department');
       return;
@@ -97,17 +103,18 @@ export default function RegisterModal() {
       return;
     }
 
-    dispatch(
-      registerUser({
-        name,
-        email,
-        contactNumber,
-        department,
-        role: 'employee',
-        password,
-      }),
-    );
+    const userData = {
+      name,
+      email,
+      contactNumber,
+      department,
+      role: 'employee',
+      password,
+    };
+
+    dispatch(registerUser(userData));
     dispatch(clearError());
+
     resetState();
 
     toast.success('Success! New employee added', {
@@ -133,6 +140,7 @@ export default function RegisterModal() {
             <input
               type='text'
               placeholder='Full name'
+              value={name}
               className={`input input-bordered w-full ${
                 errorMsg === 'name' ? 'input-error' : 'input-success'
               }`}
@@ -151,6 +159,7 @@ export default function RegisterModal() {
             <input
               type='text'
               placeholder='Email'
+              value={email}
               className={`input input-bordered w-full ${
                 errorMsg === 'email' || errorMsg === 'invalidEmail'
                   ? 'input-error'
@@ -178,6 +187,7 @@ export default function RegisterModal() {
             <input
               type='phone'
               placeholder='Contact number'
+              value={contactNumber}
               className={`input input-bordered w-full ${
                 errorMsg === 'contactNumber' ? 'input-error' : 'input-success'
               }`}
@@ -188,6 +198,14 @@ export default function RegisterModal() {
                 <span className='label-text-alt text-error'>Please enter contact number</span>
               </label>
             )}
+
+            {errorMsg === 'invalidContact' && (
+              <label className='label'>
+                <span className='label-text-alt text-error'>
+                  Contact number must be a 10-digit number
+                </span>
+              </label>
+            )}
           </div>
           <div className='form-control w-full'>
             <label className='label'>
@@ -196,6 +214,7 @@ export default function RegisterModal() {
             <input
               type='text'
               placeholder='Department'
+              value={department}
               className={`input input-bordered w-full ${
                 errorMsg === 'department' ? 'input-error' : 'input-success'
               }`}
@@ -207,18 +226,64 @@ export default function RegisterModal() {
               </label>
             )}
           </div>
-          <div className='form-control w-full'>
+          <div className='form-control w-full relative'>
             <label className='label'>
               <span className='label-text font-medium'>Password</span>
             </label>
             <input
-              type='password'
+              type={showPassword ? 'text' : 'password'}
               placeholder='Password'
-              className={`input input-bordered w-full ${
+              value={password}
+              className={`input input-bordered w-full pr-10 ${
                 errorMsg === 'password' || passwordError ? 'input-error' : 'input-success'
               }`}
               onChange={(e) => setPassword(e.target.value)}
             />
+
+            <button
+              type='button'
+              onClick={() => setShowPassword((prev) => !prev)}
+              className=' absolute top-12 right-2'
+            >
+              {showPassword ? (
+                <svg
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth={1.5}
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                  aria-hidden='true'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
+                  />
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+                  />
+                </svg>
+              ) : (
+                <svg
+                  fill='none'
+                  stroke='currentColor'
+                  strokeWidth={1.5}
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                  aria-hidden='true'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88'
+                  />
+                </svg>
+              )}
+            </button>
             {errorMsg === 'password' && (
               <label className='label'>
                 <span className='label-text-alt text-error'>Please enter password</span>
