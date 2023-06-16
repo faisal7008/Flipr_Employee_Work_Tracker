47cf0c24c68a6609ea4e5b40cc48cf45a211b065
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-17 03:09:10 +0530

    Update Tasks.jsx

--- frontend/src/pages/Employee/Tasks.jsx
+++ frontend/src/pages/Employee/Tasks.jsx
@@ -150,27 +150,6 @@ export default function Tasks() {
               clear filters
             </button>
           </div>
-          {/* <button
-            className={`btn btn-outline btn-error h-10 btn-sm`}
-            // onClick={handleDelete}
-          >
-            <svg
-              fill='none'
-              stroke='currentColor'
-              strokeWidth={2.5}
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-              aria-hidden='true'
-              className='w-5 h-5'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
-              />
-            </svg>{' '}
-            Delete
-          </button> */}
           <div className=' flex items-center justify-center sm:justify-end gap-2'>
             <label className=' font-semibold'>Tags :</label>
             <button
@@ -199,17 +178,17 @@ export default function Tasks() {
         <TaskModal />
       </div>
 
-      <div className='mt-3 h-full flex-grow overflow-auto'>
+      <div className='mt-3 h-full flex-grow scroll-container overflow-auto'>
         {filteredTasks.length > 0 ? (
           <table className='table relative w-full border-separate border-spacing-y-3'>
             <thead>
-              <tr className=' bg-transparent'>
-                <th className='sticky top-0  text-sm'>Description</th>
-                <th className='sticky top-0  text-sm'>Type</th>
-                <th className='sticky top-0  text-sm'>Date</th>
-                <th className='sticky top-0  text-sm'>Clock-In</th>
-                <th className='sticky top-0  text-sm'>Clock-Out</th>
-                <th className='sticky top-0  text-sm'>
+              <tr className='sticky top-0 bg-transparent'>
+                <th className='text-sm'>Description</th>
+                <th className='text-sm'>Type</th>
+                <th className='text-sm'>Date</th>
+                <th className='text-sm'>Clock-In</th>
+                <th className='text-sm'>Clock-Out</th>
+                <th className='text-sm'>
                   Time Taken
                   {/* <span className=' text-xs'>{'(in minutes)'}</span> */}
                 </th>
