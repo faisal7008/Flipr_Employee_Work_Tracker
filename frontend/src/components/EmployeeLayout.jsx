cb54c52896d99067bda4df8911e31d960a057174
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-17 02:54:44 +0530

    Update EmployeeLayout.jsx

--- frontend/src/components/EmployeeLayout.jsx
+++ frontend/src/components/EmployeeLayout.jsx
@@ -2,13 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from '../pages/Admin/Dashboard';
 import HomeLayout from './HomeLayout';
 import Profile from '../pages/Profile';
+import Tasks from '../pages/Employee/Tasks';
 
 export default function EmployeeLayout() {
   return (
     <Routes>
       <Route path='/' element={<HomeLayout />}>
         <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/tasks' element={<Dashboard />} />
+        <Route path='/tasks' element={<Tasks />} />
         <Route path='/projects' element={<Dashboard />} />
         <Route path='/settings' element={<Dashboard />} />
         <Route path='/profile' element={<Profile />} />
