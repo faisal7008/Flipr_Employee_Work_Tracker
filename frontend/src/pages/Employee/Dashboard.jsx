01edae47eb5155b349a73261262d2ed48d1aa41b
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-06-17 18:34:45 +0530

    employee dashboard made dynamic

--- frontend/src/pages/Employee/Dashboard.jsx
+++ frontend/src/pages/Employee/Dashboard.jsx
@@ -3,16 +3,45 @@ import { useDispatch, useSelector } from 'react-redux';
 import StackedBarChart from '../../utils/charts/StackedBarChart';
 import PieCharts from '../../utils/charts/PieCharts';
 import { getTasksByEmployee } from '../../features/task/taskSlice';
+import moment from 'moment';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
   const { tasks } = useSelector((state) => state.task);
+  // Calculate total hours
+  // const calculateTotalTime = () => {
+  let totalTime = 0;
+  let workTime = 0;
+  let meetingTime = 0;
+  let breakTime = 0;
+  tasks.forEach((task) => {
+    totalTime += task.timeTaken;
+    if (task.taskType === 'Work') workTime += task.timeTaken;
+    if (task.taskType === 'Meeting') meetingTime += task.timeTaken;
+    if (task.taskType === 'Break') breakTime += task.timeTaken;
+  });
+  // const formattedHours = moment(totalTime).format('HH:mm', { trim: false });
+  // return formattedHours;
+  // };
+  const getFormattedTime = (timeInMinutes) => {
+    if(timeInMinutes === 0){
+      return "00:00"
+    }
+    const duration = moment.duration(timeInMinutes, 'minutes');
+    // Extract hours and minutes
+    const hours = duration.hours();
+    const minutes = duration.minutes();
+    // Format the time as "hh:mm"
+    const formattedTime = moment({ hours, minutes }).format('hh:mm');
+    return formattedTime;
+  };
+
   useEffect(() => {
     dispatch(getTasksByEmployee({ employeeId: user._id }));
   }, []);
   return (
-    <div className='flex p-2 flex-col h-full scroll-container overflow-y-auto'>
+    <div className='flex p-2 md:pb-2 flex-col h-full scroll-container overflow-y-auto'>
       <div className='text-sm breadcrumbs overflow-visible'>
         <ul>
           <li>
@@ -34,7 +63,7 @@ export default function Dashboard() {
               </div>
               <div className=' flex justify-between items-end'>
                 <h2 className=' text-3xl font-semibold'>
-                  03:30 <sup className=' text-sm'>h</sup>
+                  {getFormattedTime(totalTime)} <sup className=' text-sm'>h</sup>
                 </h2>
                 <h2 className=' text-xs font-medium text-gray-300'>Total</h2>
               </div>
@@ -48,7 +77,7 @@ export default function Dashboard() {
               </div>
               <div className=' flex justify-between items-end'>
                 <h2 className=' text-3xl font-semibold'>
-                  04:24 <sup className=' text-sm'>h</sup>
+                  {getFormattedTime(workTime)} <sup className=' text-sm'>h</sup>
                 </h2>
                 <h2 className=' text-xs font-medium text-gray-300'>Total</h2>
               </div>
@@ -62,7 +91,7 @@ export default function Dashboard() {
               </div>
               <div className=' flex justify-between items-end'>
                 <h2 className=' text-3xl font-semibold'>
-                  02:15 <sup className=' text-sm'>h</sup>
+                  {getFormattedTime(meetingTime)} <sup className=' text-sm'>h</sup>
                 </h2>
                 <h2 className=' text-xs font-medium text-gray-300'>Total</h2>
               </div>
@@ -76,19 +105,19 @@ export default function Dashboard() {
               </div>
               <div className=' flex justify-between items-end'>
                 <h2 className=' text-3xl font-semibold'>
-                  01:48 <sup className=' text-sm'>h</sup>
+                  {getFormattedTime(breakTime)} <sup className=' text-sm'>h</sup>
                 </h2>
                 <h2 className=' text-xs font-medium text-gray-300'>Total</h2>
               </div>
             </div>
           </div>
         </div>
-        <div className='flex flex-col md:flex-row gap-5'>
+        <div className='flex flex-col md:flex-row pb-2 md:pb-0 gap-5'>
           <div className='w-full md:w-2/3'>
-            <StackedBarChart data={tasks} />
+            <StackedBarChart data={tasks} height={400} border={false} />
           </div>
           <div className='grow'>
-            <PieCharts data={tasks} />
+            <PieCharts data={tasks} height={400} border={false}/>
           </div>
         </div>
       </div>
