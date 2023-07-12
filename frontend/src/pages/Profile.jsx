2b77232c38f32a2745fd6ccbb0f52e8ea855fcf6
Mohammed Faisal Hussain <faisalhussain7008@gmail.com>
2023-07-13 04:57:39 +0530

    fixed resetting issue

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -40,10 +40,6 @@ export default function Profile() {
     dispatch(getMe());
   }, [dispatch]);
 
-  useEffect(() => {
-    resetProfile();
-  }, [loading]);
-
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     setProfilePicData(file);
